ebbe1a36d0502c0376362db1e933cf8e094f95c4
Vasu Singhal <vasusinghal0403@gmail.com>
2020-04-09 18:05:51 +0530

    added UML diagrams for original code

--- DASS_Assignment_3/code/Lane.java
+++ DASS_Assignment_3/code/Lane.java
@@ -185,49 +185,6 @@ public class Lane extends Thread implements PinsetterObserver {
 		this.start();
 	}
 
-//	private void continueGame() {
-//		while (gameIsHalted || gameIsPaused) {
-//			try {
-//				sleep(10);
-//			} catch (Exception e) {}
-//		}
-//
-//
-//		if (bowlerIterator.hasNext()) {
-//			currentThrower = (Bowler)bowlerIterator.next();
-//
-//			canThrowAgain = true;
-//			tenthFrameStrike = false;
-//			ball = 0;
-//			while (canThrowAgain && !gameIsPaused) {
-//				setter.ballThrown();		// simulate the thrower's ball hiting
-//				ball++;
-//			}
-//
-//			if (frameNumber == 9){
-//				finalScores[bowlIndex][gameNumber] = cumulScores[bowlIndex][9];
-//				try{
-//					Date date = new Date();
-//					String dateString = "" + date.getHours() + ":" + date.getMinutes() + " " + date.getMonth() + "/" + date.getDay() + "/" + (date.getYear() + 1900);
-//					ScoreHistoryFile.addScore(currentThrower.getNick(), dateString, new Integer(cumulScores[bowlIndex][9]).toString());
-//				} catch (Exception e) {System.err.println("Exception in addScore. "+ e );}
-//			}
-//
-//
-//			setter.reset();
-//			bowlIndex++;
-//
-//		} else {
-//			frameNumber++;
-//			resetBowlerIterator();
-//			bowlIndex = 0;
-//			if (frameNumber > 9) {
-//				gameFinished = true;
-//				gameNumber++;
-//			}
-//		}
-//	}
-
 	/** run()
 	 *
 	 * entry point for execution of this lane
